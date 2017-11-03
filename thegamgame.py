2ec4fd6dae3d8ec671a2589b55217938c22cc08d
Dustin Bench <dbench@dcsdk12.org>
2017-11-03 12:02:45 -0600

    Added comments, made a change to the username variable, and cleaned up per PEP8 styling and such.

--- thegamgame.py
+++ thegamgame.py
@@ -3,25 +3,31 @@
 import csv
 import os
 
-
-username = "Enter full DCSD email address OR type quit to close the program:\n "
+#create variables that we will call later with a raw_input function
+username = "Enter full DCSD email address: \n"
+username += 'Type "quit" at anytime to end the program\n'
 messageid = "What is the message ID you are looking for?\n "
 
 
 def gam_script():
+    """
+    - Creating a function that asks for user input to enter an email address and message id.
+    - This input is then placed into gam code and ran on the local machine.
+    - When successful, the gam script searches for the email address and deletes the message identified in the
+      messageid variable.
+    - The script runs continuously until the user types quit.
+    """
     active = True
     runthegam = "gam users " + username + " delete messages query rfc822msgid:" + messageid + " doit"
     while active:
         message_one = raw_input(username)
         if message_one == "quit":
-            active = False
             break
         message_two = raw_input(messageid)
         if message_two == "quit":
-            active = False
             break
         else:
-            print(runthegam)
+            print(runthegam + '\n')
             os.system(runthegam)
 
 
